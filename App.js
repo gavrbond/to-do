d7077816afc12974cd7414ccc2c71f865eadebbb
Bondar Gavril <gavrbondar@gmail.com>
2024-03-13 16:34:34 +0300

    todo

---
+++ App.js
@@ -0,0 +1,66 @@
+import Status from "./Components/Status/Status"
+import { useState } from "react"
+import HeaderList from "./Components/HeaderList/HeaderList"
+import Task from "./Components/Task/Task"
+import "./app.css"
+// const statusKey = {
+//   Done: "Done300",
+//   Important: "Important",
+//   inProgress: "inProgress",
+//   All: "All",
+// }
+console.log(statusKey)
+function App() {
+  const [todos, setTodos] = useState([])
+  const [status, setStatus] = useState("All")
+  const [showTime, setShowTime] = useState(false)
+  // const statusKey = {
+  //   Done: todos.filter((todo) => todo.isDone),
+  //   Important: todos.filter((todo) => todo.isImportant),
+  //   inProgress: todos.filter((todo) => !todo.isDone),
+  //   All: "All",
+  // }
+  // const searchStatus = (status) => {
+  //   return statusKey[status]
+  // }
+  // console.log(searchStatus(status))
+  return (
+    <>
+      <div className='header'>
+        <div className='container'>
+          <h1>Todo List</h1>
+          <HeaderList setTodos={setTodos} todos={todos} />
+          <Status
+            setShowTime={setShowTime}
+            status={status}
+            setStatus={setStatus}
+          />
+          <ul className='taskContainer'>
+            {todos
+              .filter((item) => {
+                if (status === "Done") {
+                  return item.isDone
+                } else if (status === "inProgress") {
+                  return !item.isDone
+                } else if (status === "Important") {
+                  return item.isImportant
+                } else {
+                  return item
+                }
+              })
+              .map((item) => (
+                <Task
+                  showTime={showTime}
+                  todos={item}
+                  key={item.id}
+                  setTodos={setTodos}
+                />
+              ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
